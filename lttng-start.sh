fa84e98ca57f1f55c5cfaf409abaa8b2503d04e2
Dipanzan Islam <dipanzan@live.com>
2022-03-30 15:47:05 -0700

    Update lttng-start.sh

--- lttng-start.sh
+++ lttng-start.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-LTTNG_TRACE_DIR="/home/dipanzan/lttng/traces"
-TRACE_SESSION_NAME="my-kernel-session"
-
+NUM_OF_TRHEADS=$1
+FILE_SIZE=$2
+NUM_OF_NETWORK_REQS=$3
+TIME=$4
 NUM_OF_RUNS=$5
 
-lttng create $TRACE_SESSION_NAME --output=$LTTNG_TRACE_DIR
+LTTNG_TRACE_DIR="/home/dipanzan/lttng/traces"
 
-# enable all syscalls and events
-lttng enable-event --kernel --all
-lttng enable-event --kernel --syscall --all
 
-lttng start
+for ((i=0;i<$5;i++))
+do
+    TRACE_SESSION_NAME="time-$TIME-threads-$NUM_OF_TRHEADS-file-$FILE_SIZE-trace-$i"
 
-# apply load
-/home/dipanzan/lttng/load.sh $@
+    lttng create $TRACE_SESSION_NAME --output=$LTTNG_TRACE_DIR
 
-lttng destroy
+    # enable all syscalls and events
+    lttng enable-event --kernel --all
+    lttng enable-event --kernel --syscall --all
 
-mv "$LTTNG_TRACE_DIR/kernel" "$LTTNG_TRACE_DIR/$TRACE_SESSION_NAME"
+    lttng start
+
+    # apply load
+    /home/dipanzan/lttng/load.sh $@
+
+    lttng destroy
+
+    mv "$LTTNG_TRACE_DIR/kernel" "$LTTNG_TRACE_DIR/$TRACE_SESSION_NAME"
+done
 
 chown -R dipanzan:dipanzan $LTTNG_TRACE_DIR
