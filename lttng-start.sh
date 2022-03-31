e87057f6abc786117e192f3f16ce28e30037e15f
Dipanzan Islam <dipanzan@live.com>
2022-03-30 18:46:22 -0700

    Update lttng-start.sh

--- lttng-start.sh
+++ lttng-start.sh
@@ -5,15 +5,16 @@ FILE_SIZE=$2
 NUM_OF_NETWORK_REQS=$3
 TIME=$4
 NUM_OF_RUNS=$5
+ENABLE_RANDOM=$6
 
 LTTNG_TRACE_DIR="/home/dipanzan/lttng/traces"
 
-
-for ((i=0;i<$5;i++))
+for ((i=0;i<$NUM_OF_RUNS;i++))
 do
-    TRACE_SESSION_NAME="time-$TIME-threads-$NUM_OF_TRHEADS-file-$FILE_SIZE-trace-$i"
+    TRACE_SESSION_NAME="time-${TIME}s-threads-$NUM_OF_TRHEADS-file-$FILE_SIZE-trace-$i-$ENABLE_RANDOM"
 
     lttng create $TRACE_SESSION_NAME --output=$LTTNG_TRACE_DIR
+    #lttng enable-channel --kernel --num-subbuf=4 --subbuf-size=256M --session=$TRACE_SESSION_NAME temp-channel
 
     # enable all syscalls and events
     lttng enable-event --kernel --all
@@ -23,7 +24,8 @@ do
 
     # apply load
     /home/dipanzan/lttng/load.sh $@
-
+    
+    #lttng disable-channel --kernel --session=$TRACE_SESSION_NAME temp-channel
     lttng destroy
 
     mv "$LTTNG_TRACE_DIR/kernel" "$LTTNG_TRACE_DIR/$TRACE_SESSION_NAME"
